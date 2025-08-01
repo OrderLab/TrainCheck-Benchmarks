fee3817f4ebbe100f944aeca5952c3b1d0e506dd
yinjie <2542374168@qq.com>
2025-08-01 10:15:18 +0800

    fix: print debug info

--- correctness_check/correct_check.py
+++ correctness_check/correct_check.py
@@ -215,9 +215,7 @@ def main():
                 print(f"Check failed for {trace_dir}")
                 all_passed = False
                 command = ["traincheck-onlinecheck", "-d", "-f", str(trace), "-i", str(invariant)]
-                process = subprocess.Popen(
-                    command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
-                )
+                process = subprocess.Popen(command)
 
                 try:
                     time.sleep(20)
